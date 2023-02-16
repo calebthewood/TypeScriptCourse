a064e4ee02f153f92edb7b82154c16b21e4f8400
calebthewood <calebwood.cs@gmail.com>
2023-02-16 11:11:51 -0800

    Section 3

--- basics.js
+++ basics.js
@@ -1,3 +1,12 @@
+// Section 2
 console.log("Hello");
 console.log("World!");
 console.log(Math.round(4.5678));
+// Section 3
+var movieTitle = "Amadeus";
+movieTitle = "Arrival";
+movieTitle = 9;
+var numCatLives = 9;
+numCatLives += 1;
+numCatLives = "0";
+var gameOver = false;
--- basics.ts
+++ basics.ts
@@ -1,3 +1,14 @@
+// Section 2
 console.log("Hello")
 console.log("World!")
 console.log(Math.round(4.5678))
+// Section 3
+let movieTitle: string = "Amadeus";
+movieTitle = "Arrival";
+movieTitle = 9;
+
+let numCatLives: number = 9;
+numCatLives += 1
+numCatLives = "0"
+
+let gameOver: boolean = false
