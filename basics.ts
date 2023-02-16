10cb32530d58a9a0cc2ae6e4106f1a8781143c1c
calebthewood <calebwood.cs@gmail.com>
2023-02-16 12:00:41 -0800

    section 4

--- basics.ts
+++ basics.ts
@@ -41,18 +41,57 @@ for (let movie of movies) {
 
 // Section 4
 
-// Always type parameters
+// Always type parameters, implied return type: number
 function square(num: number) {
   return num * num;
 }
 
-function greet(name: string) {
+// defult parameters go after annotation, implied return type: number
+function greet(name: string = "stranger") {
   return `Hello ${name}`;
 }
 
-const doSomething = (person: string, age: number, isFunny: boolean) => {}
+const doSomething = (person: string, age: number, isFunny: boolean) => { };
 
 greet(2); // err
 square(2);
-doSomething("Bob", 2, 4) // err for 3rd param
-doSomething("Bob", 2, true, 5) // err for extra param
+doSomething("Bob", 2, 4); // err for 3rd param
+doSomething("Bob", 2, true, 5); // err for extra param
+
+// Return Types
+function cube(num: number): number {
+  return num * num * num;
+}
+
+// union type
+function random(num: number): string | number {
+  if (Math.random() < 0.5) {
+    return num.toString();
+  }
+  return num;
+}
+
+// Anonymous fns. TS can use context to infer types in situations like the below.
+const colors = ["red", "blue", "green"]; // infers array of strings
+colors.map(color => {
+  return color.toUpperCase(); // infers returning array of strings
+});
+
+// Void: explicitly type void when fn will not return
+function printTwice(msg: string): void {
+  console.log(msg);
+  console.log(msg);
+  return ""
+};
+
+// Never: a function that will never return, ex: game loop, errors, etc
+const neverStop = (): never => {
+  while(true) {
+    console.log("I'm Still Going")
+  }
+  return true
+}
+
+function makeError(msg: string): never {
+  throw new Error(msg)
+}
